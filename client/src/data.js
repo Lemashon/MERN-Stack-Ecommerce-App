e3874db46571de849700eb42aeab47c30a1dfd73
Kipeno Lemayian <lemakipeno@gmail.com>
2023-03-24 22:35:43 +0300

    first commit

---
+++ client/src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://pictures-kenya.jijistatic.com/40543960_OTA4LTEyODAtMWVhYjk4MDI4MS0y.jpg",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://pictures-kenya.jijistatic.com/40543960_OTA4LTEyODAtMWVhYjk4MDI4MS0y.jpg",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://pictures-kenya.jijistatic.com/40543960_OTA4LTEyODAtMWVhYjk4MDI4MS0y.jpg",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://pictures-kenya.jijistatic.com/40543960_OTA4LTEyODAtMWVhYjk4MDI4MS0y.jpg",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://pictures-kenya.jijistatic.com/40543960_OTA4LTEyODAtMWVhYjk4MDI4MS0y.jpg",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://pictures-kenya.jijistatic.com/40543960_OTA4LTEyODAtMWVhYjk4MDI4MS0y.jpg",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://pictures-kenya.jijistatic.com/40543960_OTA4LTEyODAtMWVhYjk4MDI4MS0y.jpg",
+    },
+    {
+      id:2,
+      img:"https://pictures-kenya.jijistatic.com/40543960_OTA4LTEyODAtMWVhYjk4MDI4MS0y.jpg",
+    },
+    {
+      id:3,
+      img:"https://pictures-kenya.jijistatic.com/40543960_OTA4LTEyODAtMWVhYjk4MDI4MS0y.jpg",
+    },
+    {
+      id:4,
+      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://pictures-kenya.jijistatic.com/40543960_OTA4LTEyODAtMWVhYjk4MDI4MS0y.jpg",
+    },
+    {
+      id:8,
+      img:"https://pictures-kenya.jijistatic.com/40543960_OTA4LTEyODAtMWVhYjk4MDI4MS0y.jpg",
+    },
+  ]
